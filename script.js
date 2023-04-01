c3be3ae0c530a4020f92b714abfeb769518f6792
Prithviraj <prithvirajmahamulkar100@gmail.com>
2023-04-01 13:38:00 +0530

    change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=16&page=1';
+const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=20&page=1';
 
 fetch(apiUrl)
   .then(response => response.json())
